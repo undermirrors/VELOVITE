4e0bb447e1056ac925b9f46ee3c427e77157c3df
Guillaume Calderon <flashonfire.dev@gmail.com>
2025-01-22 10:03:16 +0100

    feat: axum init

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    tracing_subscriber::fmt::init();
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    tracing::info!("API Server is listening on port 3000");
+    axum::serve(listener, app).await.unwrap();
 }

373588ad4aae8f14d94491533b2c011fdb8530cb
Guillaume Calderon <flashonfire.dev@gmail.com>
2025-01-22 12:03:11 +0100

    feat: apply diesel migrations at app startup

--- back/src/main.rs
+++ back/src/main.rs
@@ -7,20 +7,24 @@ use axum::Router;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use std::env;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use crate::models::Stations;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
     println!("Hello, world!");
 
-    let database_url = env::var("DATABASE_URL").expect("database_url must be set");
-
-    use self::schema::stations::dsl::*;
+    let database_url = env::var("DATABASE_URL").expect("Database URL must be set");
 
     let connection = &mut PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
-
+    
+    connection.run_pending_migrations(MIGRATIONS).expect("Error applying pending migrations");
+    
+    use self::schema::stations::dsl::*;
     let results = stations
         .limit(5)
         .select(Stations::as_select())

7180b9f2b5761651521fa4226e3cd245c0811ac5
Louka <louka.pesic@gmail.com>
2025-01-22 11:25:21 +0100

    Feature: Début de contruction de la page Front

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+	let name = 'Svelte';
+	let src = "./Wink_parodie_lego_movie.png";;
+	
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h1>Hello world!</h1>
+<h1>Hello {name.toUpperCase()}!</h1>
+
+<img {src} alt="{name} winks." />
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>

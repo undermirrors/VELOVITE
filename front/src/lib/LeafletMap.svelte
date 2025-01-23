e20d706736dee0c02d8f6dc6c333a65e77d5ef91
laracine <thibaut.laracine@etu.univ-lyon1.fr>
2025-01-23 15:40:43 +0100

    fix: fix LeafletMap.svelte and add class

--- front/src/lib/LeafletMap.svelte
+++ front/src/lib/LeafletMap.svelte
@@ -1,33 +1,4 @@
 <script lang="ts">
-<<<<<<< Updated upstream
-    import 'leaflet/dist/leaflet.css';
-    import {onMount} from 'svelte';
-    import {getDetailsById, getTables} from '$lib/rust_api';
-
-    let mapContainer: string | HTMLElement;
-
-    let tables: Table[];
-
-	onMount(async () => {
-		const L = await import('leaflet');
-		const map = L.map(mapContainer, { zoomControl: false }).setView([45.74846, 4.84671], 13);
-		new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
-
-        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-        tables = await getTables();
-        tables.forEach(table => {
-            // here to add marker
-            // L.marker([table.latitude, table.longitude]).addTo(map);
-        });
-        let res: Details;
-        res = await getDetailsById(1012);
-        console.log(res);
-    });
-
-
-=======
 	import 'leaflet/dist/leaflet.css';
 	import { onMount } from 'svelte';
 
@@ -55,14 +26,13 @@
 
 		L.marker([45.7484, 4.84671], { icon: blackIcon }).addTo(map);
 	});
->>>>>>> Stashed changes
 </script>
 
 <div id="map" bind:this={mapContainer}></div>
 
 <style>
-	#map {
-		height: 600px;
-		width: 100%;
-	}
+    #map {
+        height: 600px;
+        width: 100%;
+    }
 </style>

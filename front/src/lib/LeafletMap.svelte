da45cc6bbabee6cc83f5e9777d240ed1c7680685
laracine <thibaut.laracine@etu.univ-lyon1.fr>
2025-01-23 17:33:41 +0100

    feat: markers which send station id when clicked

--- front/src/lib/LeafletMap.svelte
+++ front/src/lib/LeafletMap.svelte
@@ -1,31 +1,52 @@
 <script lang="ts">
-	import 'leaflet/dist/leaflet.css';
-	import { onMount } from 'svelte';
+    import 'leaflet/dist/leaflet.css';
+    import {onMount} from 'svelte';
+    import {getTables} from '$lib/rust_api';
 
-	let mapContainer: string | HTMLElement;
 
-	const markerLocations = [];
+    let mapContainer: string | HTMLElement;
 
-	onMount(async () => {
-		const L = await import('leaflet');
-		const map = L.map(mapContainer).setView([45.74846, 4.84671], 13);
-		map.zoomControl.setPosition('bottomright');
-		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution:
-				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
+    onMount(async () => {
 
-		//markers
-		var icon = L.Icon.extend({
-			options: {
-				iconUrl: 'marker_icon.svg'
-			}
-		});
+        const L = await import('leaflet');
 
-		var blackIcon = new icon();
+        class Icon extends L.Icon {
+            constructor() {
+                super({iconUrl: 'marker_icon.svg'});
+            }
+        }
 
-		L.marker([45.7484, 4.84671], { icon: blackIcon }).addTo(map);
-	});
+        class Markers {
+            id: number;
+            marker: L.Marker;
+
+            constructor(id: number, latitude: number, longitude: number) {
+                this.id = id;
+                const blackIcon = new Icon();
+                this.marker = L.marker([latitude, longitude], {icon: blackIcon}).addEventListener('click', () => {
+                    console.log(this.id);
+                });
+            }
+
+            getMarker() {
+                return this.marker;
+            }
+
+            getId() {
+                return this.id;
+            }
+        }
+
+        const map = L.map(mapContainer).setView([45.74846, 4.84671], 13);
+        map.zoomControl.setPosition('bottomright');
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        let markers = (await getTables()).map(table => new Markers(table.id, table.latitude, table.longitude));
+        markers.forEach(marker => marker.getMarker().addTo(map));
+    });
 </script>
 
 <div id="map" bind:this={mapContainer}></div>

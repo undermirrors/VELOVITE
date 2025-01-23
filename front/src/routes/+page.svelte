49ec68d4c15493369b097b152a613367d98c1ade
Louka <louka.pesic@gmail.com>
2025-01-23 12:34:16 +0100

    feat: adding meteo icons; adding adress and date forms; adding logo; adding close stations

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -3,11 +3,83 @@
 </script>
 
 <style>
-    div{
+    div {
         height: 100%;
+        font-family:Verdana, Tahoma, sans-serif;
+        position: relative; /* Nécessaire pour créer un contexte de positionnement */
     }
+
+    .overlay {
+        position: absolute;
+        border-style:inset;
+        border-color: grey;
+        font-size:medium;
+        z-index: 999; /* Plus élevé que la carte */
+        background-color: rgba(255, 255, 255, 1); /* Optionnel : un fond pour plus de lisibilité */
+        padding: 5px;
+        border-radius: 60px;
+        height: 60px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    }
+
+    .overlay-logo {
+        top: 20px;
+        right: 20px;
+        border-radius: 60px;
+    }
+
+    .overlay-meteo {
+        top: 20px;
+        right: 120px;
+        border-radius: 60px;
+    }
+
+    .overlay-date {
+        padding: 10px;
+        border-radius: 20px;
+        top: 20%;
+        left: 5%;
+        height: 20px;
+        width: 150px;
+    }
+
+    .overlay-adress {
+        padding: 10px;
+        border-radius: 20px;
+        top: 10%;
+        left: 5%;
+        height: 20px;
+        width: 150px;
+    }
+
+    .overlay-stations {
+        top: 30%;
+        left: 5%;
+        padding: 10px;
+        border-radius: 20px;
+        height: 200px;
+        width: 150px;
+    }
+
+
 </style>
+
 <div>
+    <input class="overlay overlay-adress" type="text" id="adress" name="adress" placeholder="Adresse" />
+     
+    <input class="overlay overlay-date" type="date" id="date" name="date" placeholder="Date"/>
+
+    <div class="overlay overlay-stations">
+        Stations proches
+    </div>
+
+    <img
+        src="/dessin.svg"
+        alt="Velovite, le site qui vous donne des vélos, vite." 
+        class="overlay overlay-logo"/>
+        
+    
+
     <LeafletMap />
 </div>
 

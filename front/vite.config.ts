5c79b6e764ef929535db5c05d1667357e10839e7
eymeric <eymericdechelette@gmail.com>
2025-01-23 12:21:11 +0100

    fix: add cors rules

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+        cors: {
+            origin: 'http://localhost:8000',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+            credentials: true
+        }
+    },
 	plugins: [sveltekit()]
 });

90e9cffb9cb77fa6cccf0e34b8dfd5ababb13c3a
Lea Michel <lm.michel63@gmail.com>
2025-01-23 10:35:08 +0100

    feat: include leaflet map

---
+++ front/src/lib/MarkerPopup.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	
+	/**
+	 * @type {any}
+	 */
+	 export let count;
+	
+	/**
+	 * @param {number} delta
+	 */
+	function addValue(delta) {
+		count += delta;
+		dispatch('change', count);
+	}
+</script>
+
+<style>
+	button {
+		width: 2rem
+	}
+</style>
+
+<div>
+	<button on:click={() => addValue(-1)}>
+		-
+	</button>
+	<button on:click={() => addValue(1)}>
+		+
+	</button>	
+</div>
+
+<div style="width:100%;text-align:center;font-weight:600">
+	{count}
+</div>

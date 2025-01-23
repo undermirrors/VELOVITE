e1f3d9bbd00b7fb7c41f644c05f1bd56d0fb9024
Lea Michel <lm.michel63@gmail.com>
2025-01-23 10:35:08 +0100

    feat: include leaflet map

---
+++ front/src/lib/LeafletMap.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import 'leaflet/dist/leaflet.css';
+    import { onMount } from 'svelte';
+
+  let mapContainer: string | HTMLElement;
+
+  onMount(async () => {
+    const L = await import('leaflet');
+    const map = L.map(mapContainer).setView([45.74846000, 4.84671000], 13);
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+  });
+</script>
+
+     <style>
+        #map { height: 600px; width: 100%; }
+     </style>
+
+<div id="map" bind:this={mapContainer}></div>
+
+
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,53 +1,13 @@
-<script lang="ts">
-	import L from 'leaflet';
-	import MapToolbar from './MapToolbar.svelte';
-	import MarkerPopup from './MarkerPopup.svelte';
-	import * as markerIcons from './markers.js';
-	let map;
-
-	const markerLocations = [
-		[29.8283, -96.5795],
-		[37.8283, -90.5795],
-		[43.8283, -102.5795],
-		[48.40, -122.5795],
-		[43.60, -79.5795],
-		[36.8283, -100.5795],
-		[38.40, -122.5795],
-	];
-
-	const initialView = [39.8283, -98.5795];
-	function createMap(container) {
-		let m = L.map(container, { preferCanvas: true }).setView(initialView, 5);
-		L.tileLayer(
-			'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-			{
-				attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
-				&copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
-				subdomains: 'abcd',
-				maxZoom: 14,
-			}
-		).addTo(m);
-
-		return m;
-	}
-
-	let toolbar = L.control({ position: 'topright' });
-	let toolbarComponent;
-	toolbar.onAdd = (map) => {
-		let div = L.DomUtil.create('div');
-		toolbarComponent = new MapToolbar({
-			target: div,
-			props: {}
-		});
-
-		toolbarComponent.$on('click-eye', ({ detail }) => eye = detail);
-		toolbarComponent.$on('click-lines', ({ detail }) => lines = detail);
-		toolbarComponent.$on('click-reset', () => {
-			if (map) {
-				map.setView(initialView, 5, { animate: true });
-			}
-		});
-
-		return div;
-	};
+<script>
+	import LeafletMap from '$lib/LeafletMap.svelte';
 </script>
+
+<style>
+    div{
+        height: 100%;
+    }
+</style>
+<div>
+    <LeafletMap />
+</div>
+

aee652ae6aceb88ac2446d51ac0b6d12e78696b6
Guillaume Calderon <flashonfire.dev@gmail.com>
2025-01-23 11:32:25 +0100

    feat: basic stations + mock

--- back/src/mock.rs
+++ back/src/mock.rs
@@ -1,10 +1,26 @@
-use crate::models::Station;
+use crate::models::{BasicStation, DetailedStation};
 use axum::Json;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref STATIONS_MOCK: [Station; 2] = [
-        Station {
+    static ref STATIONS_MOCK: [BasicStation; 2] = [
+        BasicStation {
+            id: 0,
+            latitude: 26.0,
+            longitude: 72.0,
+        },
+        BasicStation {
+            id: 1,
+            latitude: 4.0,
+            longitude: 12.0,
+        }
+    ];
+}
+
+
+lazy_static! {
+    static ref DETAILED_STATIONS_MOCK: [DetailedStation; 2] = [
+        DetailedStation {
             id: 0,
             name: "oui".to_string(),
             latitude: 26.0,
@@ -13,7 +29,7 @@ lazy_static! {
             area: "oui area".to_string(),
             capacity: 5,
         },
-        Station {
+        DetailedStation {
             id: 1,
             name: "non".to_string(),
             latitude: 4.0,
@@ -25,6 +41,11 @@ lazy_static! {
     ];
 }
 
-pub fn get_stations_mock() -> Json<&'static [Station; 2]> {
+
+pub fn get_detailed_stations_mock() -> Json<&'static [DetailedStation; 2]> {
+    Json(&*DETAILED_STATIONS_MOCK)
+}
+
+pub fn get_stations_mock() -> Json<&'static [BasicStation; 2]> {
     Json(&*STATIONS_MOCK)
 }
--- back/src/models.rs
+++ back/src/models.rs
@@ -13,7 +13,7 @@ pub struct Forecast {
 #[derive(Queryable, Selectable, Serialize, Deserialize, Insertable, Debug, Clone)]
 #[diesel(table_name = crate::schema::station)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct Station {
+pub struct DetailedStation {
     pub id: i32,
     pub name: String,
     pub latitude: f64,
@@ -21,4 +21,13 @@ pub struct Station {
     pub adress: String,
     pub area: String,
     pub capacity: i32,
+}
+
+#[derive(Queryable, Selectable, Serialize, Deserialize, Insertable, Debug, Clone)]
+#[diesel(table_name = crate::schema::station)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct BasicStation {
+    pub id: i32,
+    pub latitude: f64,
+    pub longitude: f64,
 }

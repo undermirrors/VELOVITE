13af4a20af38cfffde8e7b19f29e5f3a048f5491
eymeric <eymericdechelette@gmail.com>
2025-01-23 16:13:40 +0100

    fix: rollback to datetime utc

--- back/src/downloader.rs
+++ back/src/downloader.rs
@@ -1,6 +1,5 @@
 use ::serde::{Deserialize, Serialize};
-use chrono::NaiveDateTime;
-use serde::Deserializer;
+use chrono::Utc;
 use tracing::{error, info};
 
 pub async fn downloader_data() {
@@ -73,8 +72,8 @@ pub struct Root {
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Value {
-    #[serde(deserialize_with = "date_format")]
-    pub horodate: chrono::NaiveDateTime,
+    #[serde(with = "date_format")]
+    pub horodate: chrono::DateTime<Utc>,
     #[serde(rename = "main_stands")]
     pub main_stands: MainStands,
     pub number: u16,
@@ -117,13 +116,40 @@ pub struct TotalStands {
     pub capacity: u16,
 }
 
-const FORMAT: &str = "%Y-%m-%d %H:%M:%S%:z";
+mod date_format {
+    use chrono::{DateTime, NaiveDateTime, Utc};
+    use serde::{self, Deserialize, Deserializer, Serializer};
 
-pub fn date_format<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    let dt = NaiveDateTime::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)?;
-    Ok(dt)
+    const FORMAT: &str = "%Y-%m-%d %H:%M:%S%:z";
+
+    // The signature of a serialize_with function must follow the pattern:
+    //
+    //    fn serialize<S>(&T, S) -> Result<S::Ok, S::Error>
+    //    where
+    //        S: Serializer
+    //
+    // although it may also be generic over the input types T.
+    pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    // The signature of a deserialize_with function must follow the pattern:
+    //
+    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
+    //    where
+    //        D: Deserializer<'de>
+    //
+    // although it may also be generic over the output types T.
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let dt = NaiveDateTime::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)?;
+        Ok(DateTime::<Utc>::from_naive_utc_and_offset(dt, Utc))
+    }
 }

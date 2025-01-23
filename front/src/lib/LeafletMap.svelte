5219cd9d84c3d9d08374c062d06df56473a3fbe9
Lea Michel <lm.michel63@gmail.com>
2025-01-23 15:23:02 +0100

    feat: add marker icon on map

--- front/src/lib/LeafletMap.svelte
+++ front/src/lib/LeafletMap.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+<<<<<<< Updated upstream
     import 'leaflet/dist/leaflet.css';
     import {onMount} from 'svelte';
     import {getDetailsById, getTables} from '$lib/rust_api';
@@ -26,6 +27,35 @@
     });
 
 
+=======
+	import 'leaflet/dist/leaflet.css';
+	import { onMount } from 'svelte';
+
+	let mapContainer: string | HTMLElement;
+
+	const markerLocations = [];
+
+	onMount(async () => {
+		const L = await import('leaflet');
+		const map = L.map(mapContainer).setView([45.74846, 4.84671], 13);
+		map.zoomControl.setPosition('bottomright');
+		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		//markers
+		var icon = L.Icon.extend({
+			options: {
+				iconUrl: 'marker_icon.svg'
+			}
+		});
+
+		var blackIcon = new icon();
+
+		L.marker([45.7484, 4.84671], { icon: blackIcon }).addTo(map);
+	});
+>>>>>>> Stashed changes
 </script>
 
 <div id="map" bind:this={mapContainer}></div>
--- front/src/lib/Markers.svelte
+++ front/src/lib/Markers.svelte
@@ -1,2 +1,5 @@
-// Icons from heroicons.dev
-export const library = `<svg style="width:30px;height:30px" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"></path></svg>`;
+<svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M42 20C42 34 24 46 24 46C24 46 6 34 6 20C6 15.2261 7.89642 10.6477 11.2721 7.27208C14.6477 3.89642 19.2261 2 24 2C28.7739 2 33.3523 3.89642 36.7279 7.27208C40.1036 10.6477 42 15.2261 42 20Z" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M24 26C27.3137 26 30 23.3137 30 20C30 16.6863 27.3137 14 24 14C20.6863 14 18 16.6863 18 20C18 23.3137 20.6863 26 24 26Z" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    

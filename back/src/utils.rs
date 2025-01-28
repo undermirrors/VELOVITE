ab6c997a9af5d0f518744220230ddb791768c4cb
eymeric <eymericdechelette@gmail.com>
2025-01-28 12:35:00 +0100

    feat: coef on prediction coefs

--- back/src/utils.rs
+++ back/src/utils.rs
@@ -1,10 +1,16 @@
 use crate::learning::MergedData;
 
+const HOUR_COEF: f32 = 1.0;
+const DAY_COEF: f32 = 1.0;
+const TEMPERATURE_COEF: f32 = 1.0;
+const PRECIPITATION_COEF: f32 = 1.0;
+const WIND_SPEED_COEF: f32 = 1.0;
+
 pub fn distance(a: &MergedData, b: &MergedData) -> f32 {
-    (((a.hour - b.hour) as f32 / 24.0).powf(2.0)
-        + ((a.day - b.day) as f32 / 7.0).powf(2.0)
-        + ((a.temperature - b.temperature) / 40.0).powf(2.0)
-        + ((a.precipitation - b.precipitation) / 100.0).powf(2.0)
-        + ((a.wind_speed - b.wind_speed) / 100.0).powf(2.0))
+    ((HOUR_COEF * ((a.hour - b.hour) as f32 / 24.0).powf(2.0))
+        + (DAY_COEF * ((a.day - b.day) as f32 / 7.0).powf(2.0))
+        + (TEMPERATURE_COEF * ((a.temperature - b.temperature) / 40.0).powf(2.0))
+        + (PRECIPITATION_COEF * ((a.precipitation - b.precipitation) / 100.0).powf(2.0))
+        + (WIND_SPEED_COEF * ((a.wind_speed - b.wind_speed) / 100.0).powf(2.0)))
     .sqrt()
 }
